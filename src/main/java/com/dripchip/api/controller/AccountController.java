e552d51c476db36abd3254741a974df5aa672a00
Vladimir Kidyaev <krios2146@gmail.com>
2023-03-14 22:49:47 +0700

    feat: implement search query in the AccountController

--- src/main/java/com/dripchip/api/controller/AccountController.java
+++ src/main/java/com/dripchip/api/controller/AccountController.java
@@ -1,12 +1,18 @@
 package com.dripchip.api.controller;
 
-import com.dripchip.api.model.Account;
+import com.dripchip.api.entity.Account;
+import com.dripchip.api.entity.specification.AccountSpecifications;
 import com.dripchip.api.repository.AccountRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -19,6 +25,11 @@ public class AccountController {
 
     @GetMapping("/accounts/{accountId}")
     public ResponseEntity<Account> getAccountById(@PathVariable Long accountId) {
+
+        if (accountId < 1) {
+            return ResponseEntity.badRequest().build();
+        }
+
         Optional<Account> account = accountRepository.findById(accountId);
 
         if (account.isEmpty()) {
@@ -27,4 +38,32 @@ public class AccountController {
 
         return ResponseEntity.ok().body(account.get());
     }
+
+    @GetMapping("/accounts/search")
+    public ResponseEntity<List<Account>> searchAccounts(
+            @RequestParam(required = false) String firstName,
+            @RequestParam(required = false) String lastName,
+            @RequestParam(required = false) String email,
+            @RequestParam(defaultValue = "0") int from,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+
+        if (size < 1 || from < 0) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        Specification<Account> spec = Specification.where(AccountSpecifications.firstNameContainsIgnoreCase(firstName)
+                .and(AccountSpecifications.lastNameContainsIgnoreCase(lastName))
+                .and(AccountSpecifications.emailContainsIgnoreCase(email))
+        );
+
+        Page<Account> page = accountRepository.findAll(spec, PageRequest.of(from, size));
+
+        if (page.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok().body(page.getContent());
+    }
+
 }
