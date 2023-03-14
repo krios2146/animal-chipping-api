fff542f09ebc7c7d4d893915125ad376c4483f21
Vladimir Kidyaev <krios2146@gmail.com>
2023-03-14 22:49:02 +0700

    feat: add JpaSpecificationExecutor to AccountRepository

--- src/main/java/com/dripchip/api/repository/AccountRepository.java
+++ src/main/java/com/dripchip/api/repository/AccountRepository.java
@@ -1,8 +1,9 @@
 package com.dripchip.api.repository;
 
-import com.dripchip.api.model.Account;
+import com.dripchip.api.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface AccountRepository extends JpaRepository<Account, Long> {
+public interface AccountRepository extends JpaRepository<Account, Long>, JpaSpecificationExecutor<Account> {
 
 }
