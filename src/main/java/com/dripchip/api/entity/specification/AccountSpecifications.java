675b1d022273856b2a0ee62367caa3cb032b119a
Vladimir Kidyaev <krios2146@gmail.com>
2023-03-14 22:49:14 +0700

    feat: add AccountSpecifications class

---
+++ src/main/java/com/dripchip/api/entity/specification/AccountSpecifications.java
@@ -0,0 +1,46 @@
+package com.dripchip.api.entity.specification;
+
+import com.dripchip.api.entity.Account;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import org.springframework.data.jpa.domain.Specification;
+
+public class AccountSpecifications {
+    public static Specification<Account> firstNameContainsIgnoreCase(String firstName) {
+        return new Specification<Account>() {
+            @Override
+            public Predicate toPredicate(Root<Account> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+                if (firstName == null) {
+                    return null;
+                }
+                return criteriaBuilder.like(criteriaBuilder.lower(root.get("firstName")), "%" + firstName.toLowerCase() + "%");
+            }
+        };
+    }
+
+    public static Specification<Account> lastNameContainsIgnoreCase(String lastName) {
+        return new Specification<Account>() {
+            @Override
+            public Predicate toPredicate(Root<Account> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+                if (lastName == null) {
+                    return null;
+                }
+                return criteriaBuilder.like(criteriaBuilder.lower(root.get("lastName")), "%" + lastName.toLowerCase() + "%");
+            }
+        };
+    }
+
+    public static Specification<Account> emailContainsIgnoreCase(String email) {
+        return new Specification<Account>() {
+            @Override
+            public Predicate toPredicate(Root<Account> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+                if (email == null) {
+                    return null;
+                }
+                return criteriaBuilder.like(criteriaBuilder.lower(root.get("email")), "%" + email.toLowerCase() + "%");
+            }
+        };
+    }
+}
