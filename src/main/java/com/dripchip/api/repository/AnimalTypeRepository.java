660112b2504e97f067b53ac1dfaf41780c213b3e
Vladimir Kidyaev <krios2146@gmail.com>
2023-03-15 12:53:45 +0700

    feat: add AnimalTypeRepository

---
+++ src/main/java/com/dripchip/api/repository/AnimalTypeRepository.java
@@ -0,0 +1,8 @@
+package com.dripchip.api.repository;
+
+import com.dripchip.api.entity.AnimalType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnimalTypeRepository extends JpaRepository<AnimalType, Long> {
+
+}
