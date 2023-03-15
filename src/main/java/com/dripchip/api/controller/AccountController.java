8a66d3eb418867c264beababec67748bcbaafede
Vladimir Kidyaev <krios2146@gmail.com>
2023-03-15 15:31:41 +0700

    feat: implement POST method in the AccountController

--- src/main/java/com/dripchip/api/controller/AccountController.java
+++ src/main/java/com/dripchip/api/controller/AccountController.java
@@ -3,14 +3,13 @@ package com.dripchip.api.controller;
 import com.dripchip.api.entity.Account;
 import com.dripchip.api.entity.specification.AccountSpecifications;
 import com.dripchip.api.repository.AccountRepository;
+import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -63,6 +62,19 @@ public class AccountController {
         }
 
         return ResponseEntity.ok().body(accounts.getContent());
+    }
+
+    @PostMapping("/registration")
+    public ResponseEntity<Account> registerAccount(@Valid @RequestBody Account account) {
+        Optional<Account> accountOptional = accountRepository.findByEmail(account.getEmail());
+
+        if (accountOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+
+        accountRepository.save(account);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(account);
     }
 
 }
