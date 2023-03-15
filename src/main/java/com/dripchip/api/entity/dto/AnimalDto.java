f55bc5ac233d081584098706d58f4b56a8079410
Vladimir Kidyaev <krios2146@gmail.com>
2023-03-15 20:45:17 +0700

    feat: update AnimalDto with new fields for the response

--- src/main/java/com/dripchip/api/entity/dto/AnimalDto.java
+++ src/main/java/com/dripchip/api/entity/dto/AnimalDto.java
@@ -1,18 +1,27 @@
 package com.dripchip.api.entity.dto;
 
+import com.dripchip.api.entity.enums.Gender;
+import com.dripchip.api.entity.enums.LifeStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.util.Set;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 public class AnimalDto {
-    private Set<Long> animalTypesId;
+    private Long id;
+    private List<Long> animalTypes;
     private Double weight;
     private Double length;
     private Double height;
-    private String gender;
+    private Gender gender;
+    private LifeStatus lifeStatus;
+    private OffsetDateTime chippingDateTime;
     private Long chipperId;
     private Long chippingLocationId;
+    private List<Long> visitedLocations;
+    private LocalDateTime deathDateTime;
 }
