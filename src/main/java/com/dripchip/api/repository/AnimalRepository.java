8289eb4bbb36c61645b67d6959a0a0239133b8d9
Vladimir Kidyaev <krios2146@gmail.com>
2023-03-15 12:23:05 +0700

    feat: AnimalRepository now extends JpaSpecificationExecutor

--- src/main/java/com/dripchip/api/repository/AnimalRepository.java
+++ src/main/java/com/dripchip/api/repository/AnimalRepository.java
@@ -2,7 +2,8 @@ package com.dripchip.api.repository;
 
 import com.dripchip.api.entity.Animal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface AnimalRepository extends JpaRepository<Animal, Long> {
+public interface AnimalRepository extends JpaRepository<Animal, Long>, JpaSpecificationExecutor<Animal> {
 
 }
