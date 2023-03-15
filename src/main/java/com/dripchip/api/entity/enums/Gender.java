c896a0e60ad68e67730474a3966a8ab8ceecb5f3
Vladimir Kidyaev <krios2146@gmail.com>
2023-03-15 07:43:54 +0700

    feat: add Gender & LifeStatus enums for the Animal entity

---
+++ src/main/java/com/dripchip/api/entity/enums/Gender.java
@@ -0,0 +1,7 @@
+package com.dripchip.api.entity.enums;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    OTHER
+}
---
+++ src/main/java/com/dripchip/api/entity/enums/LifeStatus.java
@@ -0,0 +1,6 @@
+package com.dripchip.api.entity.enums;
+
+public enum LifeStatus {
+    ALIVE,
+    DEAD
+}
