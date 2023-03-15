bba15cd9b4e85dbfcd382aafdbb22d2bcaa44c1b
Vladimir Kidyaev <krios2146@gmail.com>
2023-03-15 15:31:19 +0700

    feat: add findByEmail method to the AccountRepository

--- src/main/java/com/dripchip/api/repository/AccountRepository.java
+++ src/main/java/com/dripchip/api/repository/AccountRepository.java
@@ -4,6 +4,8 @@ import com.dripchip.api.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface AccountRepository extends JpaRepository<Account, Long>, JpaSpecificationExecutor<Account> {
+import java.util.Optional;
 
+public interface AccountRepository extends JpaRepository<Account, Long>, JpaSpecificationExecutor<Account> {
+    Optional<Account> findByEmail(String email);
 }
