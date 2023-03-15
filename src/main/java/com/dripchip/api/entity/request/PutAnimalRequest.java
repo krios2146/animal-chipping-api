a4fe7640d441858051e340dc5122cce1e21233de
Vladimir Kidyaev <krios2146@gmail.com>
2023-03-15 22:42:18 +0700

    refactor: add PutAnimalRequest as subclass of AnimalRequest

---
+++ src/main/java/com/dripchip/api/entity/request/PutAnimalRequest.java
@@ -0,0 +1,14 @@
+package com.dripchip.api.entity.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@NoArgsConstructor
+public class PutAnimalRequest extends AnimalRequest {
+    @NotNull
+    private String lifeStatus;
+}
