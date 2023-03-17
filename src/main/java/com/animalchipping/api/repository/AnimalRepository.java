6f38dc48c1b2b9a22119c5d5484ac531a2f0e364
Vladimir Kidyaev <krios2146@gmail.com>
2023-03-17 19:45:29 +0700

    feat: add existsByChippingLocation method to the AnimalRepository

--- src/main/java/com/animalchipping/api/repository/AnimalRepository.java
+++ src/main/java/com/animalchipping/api/repository/AnimalRepository.java
@@ -2,9 +2,11 @@ package com.animalchipping.api.repository;
 
 import com.animalchipping.api.entity.Account;
 import com.animalchipping.api.entity.Animal;
+import com.animalchipping.api.entity.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface AnimalRepository extends JpaRepository<Animal, Long>, JpaSpecificationExecutor<Animal> {
     boolean existsByChipper(Account account);
+    boolean existsByChippingLocation(Location location);
 }
