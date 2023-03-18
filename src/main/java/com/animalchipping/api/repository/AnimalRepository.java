7c6faa62db5f3680df2100632bab0e0689f6d404
Vladimir Kidyaev <krios2146@gmail.com>
2023-03-18 20:07:55 +0700

    feat: add existsByAnimalType method to the AnimalRepository

--- src/main/java/com/animalchipping/api/repository/AnimalRepository.java
+++ src/main/java/com/animalchipping/api/repository/AnimalRepository.java
@@ -2,11 +2,26 @@ package com.animalchipping.api.repository;
 
 import com.animalchipping.api.entity.Account;
 import com.animalchipping.api.entity.Animal;
+import com.animalchipping.api.entity.AnimalType;
 import com.animalchipping.api.entity.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AnimalRepository extends JpaRepository<Animal, Long>, JpaSpecificationExecutor<Animal> {
     boolean existsByChipper(Account account);
+
     boolean existsByChippingLocation(Location location);
+
+    @Query("SELECT CASE " +
+            "WHEN COUNT(a) > 0 THEN true " +
+            "ELSE false " +
+            "END " +
+            "FROM Animal a " +
+            "WHERE EXISTS " +
+            "(SELECT at FROM AnimalType at " +
+            "WHERE at.type = :#{#type.type} " +
+            "AND at.animal = a)")
+    boolean existsByAnimalType(@Param("type") AnimalType type);
 }
