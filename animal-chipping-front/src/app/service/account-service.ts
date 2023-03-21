d5ce4f0c1476c0f27ea0abb2684198f5df7acf62
Vladimir Kidyaev <krios2146@gmail.com>
2023-03-21 18:46:23 +0700

    feat: add account-service.ts for sending requests to the API

---
+++ animal-chipping-front/src/app/service/account-service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AccountRequest } from '../model/account-request';
+import { AccountResponse } from '../model/account-response';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  private apiUrl = 'http://localhost:8080/accounts';
+
+  constructor(private http: HttpClient) {}
+
+  getAccountById(accountId: number): Observable<AccountResponse> {
+    return this.http.get<AccountResponse>(`${this.apiUrl}/${accountId}`);
+  }
+
+  updateAccount(accountData: AccountRequest): Observable<AccountResponse> {
+    return this.http.put<AccountResponse>(this.apiUrl, accountData);
+  }
+
+  deleteAccount(accountId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${accountId}`);
+  }
+  
+}
