d34550880cf0facf3775014936326bfe08e73571
Vladimir Kidyaev <krios2146@gmail.com>
2023-03-21 19:34:29 +0700

    feat: add simple account component

--- animal-chipping-front/src/app/app.module.ts
+++ animal-chipping-front/src/app/app.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AccountComponent } from './account/account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+    AccountComponent
+  ]
 })
 export class AppModule { }
