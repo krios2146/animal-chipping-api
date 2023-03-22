7f63f2b01bdfe1a20cfeb0387f8a6c44ae237e25
Vladimir Kidyaev <krios2146@gmail.com>
2023-03-22 17:34:40 +0700

    feat: add routing for the animal type component

--- animal-chipping-front/src/app/app-routing.module.ts
+++ animal-chipping-front/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccountComponent } from './account/account.component';
 import { AnimalComponent } from './animal/animal.component';
+import { AnimalTypeComponent } from './animal-type/animal-type.component';
 
 const routes: Routes = [
   { path: 'account', component: AccountComponent },
   { path: 'animal', component: AnimalComponent },
+  { path: 'animal-type', component: AnimalTypeComponent }
 ];
 
 @NgModule({
