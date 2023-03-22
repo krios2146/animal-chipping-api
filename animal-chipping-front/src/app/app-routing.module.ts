aa715876ac2641216e63517a85df0f23877adc75
Vladimir Kidyaev <krios2146@gmail.com>
2023-03-22 14:07:17 +0700

    feat; change main page of the app

--- animal-chipping-front/src/app/app-routing.module.ts
+++ animal-chipping-front/src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { AccountComponent } from './account/account.component';
 
 const routes: Routes = [
-  { path: '', component: AppComponent },
   { path: 'account', component: AccountComponent }
 ];
 
