b6e840a87649d994c23d6447ba8fbaf094ad31ef
Vladimir Kidyaev <krios2146@gmail.com>
2023-03-23 14:37:53 +0700

    feat: add logic to the find animal type form

--- animal-chipping-front/src/app/animal-type/animal-type.component.ts
+++ animal-chipping-front/src/app/animal-type/animal-type.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AnimalTypeService } from '../service/animal-type.service';
+import { AnimalTypeResponse } from '../model/animal-type/animal-type-response';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-animal-type',
@@ -6,5 +10,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./animal-type.component.css']
 })
 export class AnimalTypeComponent {
+  findForm: FormGroup;
 
+  foundedAnimalType: AnimalTypeResponse | undefined;
+
+  constructor(private formBuilder: FormBuilder, private animalTypeService: AnimalTypeService) {
+    this.findForm = this.formBuilder.group({
+      typeId: [null, Validators.required]
+    })
+  }
+
+  findAnimalType() {
+    const typeId = this.findForm.get('typeId')!.value;
+    this.animalTypeService.getAnimalTypeById(typeId).subscribe(
+      (response: AnimalTypeResponse) => {
+        this.foundedAnimalType = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
 }
