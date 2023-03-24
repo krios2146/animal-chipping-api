7224bf5222b887a57ecbec766f5af84b5924688d
Vladimir Kidyaev <krios2146@gmail.com>
2023-03-24 11:24:46 +0700

    feat: update animal models with using enums

--- animal-chipping-front/src/app/model/animal/animal-response.ts
+++ animal-chipping-front/src/app/model/animal/animal-response.ts
@@ -1,11 +1,14 @@
+import { Gender } from "./enum/gender.enum";
+import { LifeStatus } from "./enum/life-status.enum";
+
 export interface AnimalResponse {
   id: number;
   animalTypes: number[];
   weight: number;
   length: number;
   height: number;
-  gender: string;
-  lifeStatus: string;
+  gender: Gender;
+  lifeStatus: LifeStatus;
   chippingDateTime: string;
   chipperId: number;
   chippingLocationId: number;
