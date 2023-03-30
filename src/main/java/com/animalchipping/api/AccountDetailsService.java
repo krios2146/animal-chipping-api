41330e48ce999d6fa21d5dd72ac4bf4aaae9730b
Vladimir Kidyaev <krios2146@gmail.com>
2023-03-30 19:24:53 +0700

    feat: implement AccountDetailsService

---
+++ src/main/java/com/animalchipping/api/AccountDetailsService.java
@@ -0,0 +1,33 @@
+package com.animalchipping.api;
+
+import com.animalchipping.api.entity.Account;
+import com.animalchipping.api.repository.AccountRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@Service
+public class AccountDetailsService implements UserDetailsService {
+    private final AccountRepository accountRepository;
+
+    public AccountDetailsService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<Account> accountOptional = accountRepository.findByEmail(email);
+        Account account = accountOptional.orElseThrow(RuntimeException::new);
+
+        return new User(
+                account.getEmail(),
+                account.getPassword(),
+                new ArrayList<>()
+        );
+    }
+}
