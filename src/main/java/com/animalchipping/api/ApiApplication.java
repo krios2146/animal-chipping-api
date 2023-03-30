a1ce233c3be99169b7f99c9299fdf5b78c4b79c2
Vladimir Kidyaev <krios2146@gmail.com>
2023-03-30 19:25:30 +0700

    feat: add pre-registered root account, for testing and demonstrating purposes

--- src/main/java/com/animalchipping/api/ApiApplication.java
+++ src/main/java/com/animalchipping/api/ApiApplication.java
@@ -1,13 +1,27 @@
 package com.animalchipping.api;
 
+import com.animalchipping.api.entity.Account;
+import com.animalchipping.api.repository.AccountRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiApplication {
 
+    @Bean
+    public CommandLineRunner commandLineRunner(AccountRepository accountRepository) {
+        return args -> accountRepository.save(
+                new Account("RootFirst",
+                        "RootLast",
+                        "root@email.com",
+                        "root"
+                )
+        );
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
     }
-
 }
