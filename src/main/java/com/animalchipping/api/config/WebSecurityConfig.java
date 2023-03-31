3771b32c8ce409112719bd27ff5281baabba09fb
Vladimir Kidyaev <krios2146@gmail.com>
2023-03-31 14:54:17 +0700

    chore: move WebSecurityConfig to a config package

--- src/main/java/com/animalchipping/api/config/WebSecurityConfig.java
+++ src/main/java/com/animalchipping/api/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.animalchipping.api;
+package com.animalchipping.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
