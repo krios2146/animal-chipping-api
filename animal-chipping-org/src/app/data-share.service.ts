2dd98f4b8da1d64b95d309b9a68008418c3e7a46
Vladimir Kidyaev <krios2146@gmail.com>
2023-04-05 19:35:27 +0700

    feat: aupdate data-share service with additional filed

--- animal-chipping-org/src/app/data-share.service.ts
+++ animal-chipping-org/src/app/data-share.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { AccountResponse } from './account/account-response';
+import { AccountRequest } from './account/account-request';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataShareService {
+  private account: AccountRequest | undefined;
+  private accountId: number | undefined;
 
   constructor() { }
+
+  setAccount(account: AccountRequest) {
+    this.account = account;
+  }
+
+  getAccount(): AccountRequest {
+    return this.account!;
+  }
+
+  setAccountId(accountId: number) {
+    this.accountId = accountId;
+  }
+
+  getAccountId(): number {
+    return this.accountId!;
+  }
 }
