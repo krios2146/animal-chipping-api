247d56df7714a079457355467ffabae092cccae4
Vladimir Kidyaev <krios2146@gmail.com>
2023-04-05 14:24:43 +0700

    feat: add animal-registration component

---
+++ animal-chipping-org/src/app/animal-registration/animal-registration.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-animal-registration',
+  templateUrl: './animal-registration.component.html',
+  styleUrls: ['./animal-registration.component.css']
+})
+export class AnimalRegistrationComponent {
+
+}
