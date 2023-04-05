43b22b6d3bb5323df4a174b8bc3ad5fe8cb959ef
Vladimir Kidyaev <krios2146@gmail.com>
2023-04-05 13:43:34 +0700

    feat: add home component

--- animal-chipping-org/src/app/app.module.ts
+++ animal-chipping-org/src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { Router, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
