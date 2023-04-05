53c3cc37ebc412d243dd77282a076d234045d103
Vladimir Kidyaev <krios2146@gmail.com>
2023-04-05 14:25:56 +0700

    feat: add routing for new components

--- animal-chipping-org/src/app/app-routing.module.ts
+++ animal-chipping-org/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { AnimalRegistrationComponent } from './animal-registration/animal-registration.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'registration', component: RegistrationComponent },
+  { path: 'animal-registration', component: AnimalRegistrationComponent },
 ];
 
 @NgModule({
