a1583e80a05899f799140baef4691a4683bacc52
Vladimir Kidyaev <krios2146@gmail.com>
2023-04-05 19:23:52 +0700

    feat: update animal-registration component with logic

--- animal-chipping-org/src/app/animal-registration/animal-registration.component.ts
+++ animal-chipping-org/src/app/animal-registration/animal-registration.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { AnimalRegistrationService } from '../animal-registration.service';
+import { AnimalResponse } from '../animal/animal-response';
+import { Gender } from '../animal/enum/gender.enum';
+import { AnimalCreateRequest } from '../animal/animal-create-request';
+import { HttpErrorResponse } from '@angular/common/http';
+import { RegistrationComponent } from '../registration/registration.component';
 
 @Component({
   selector: 'app-animal-registration',
@@ -6,5 +13,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./animal-registration.component.css']
 })
 export class AnimalRegistrationComponent {
+  registerAnimalForm: FormGroup;
 
+  private registeredUserId: number;
+
+  registeredAnimal: AnimalResponse | undefined;
+
+  constructor(private formBuilder: FormBuilder, private animalRegistrationService: AnimalRegistrationService, private registration: RegistrationComponent) {
+    this.registerAnimalForm = this.formBuilder.group({
+      weight: [null, [Validators.required, Validators.min(1)]],
+      gender: [null, [Validators.required, this.enumValidator(Gender)]],
+    });
+
+    this.registeredUserId = registration.registeredAccount!.id;
+  }
+
+  registerAnimal() {
+    const animal: AnimalCreateRequest = {
+      weight: this.registerAnimalForm.get('weight')?.value,
+      gender: this.registerAnimalForm.get('gender')?.value,
+      chipperId: this.registeredUserId,
+      length : 10,
+      height : 10,
+      chippingLocationId : 1,
+      animalTypes: [1]
+    };
+
+    this.animalRegistrationService.registerAnimal(animal).subscribe(
+      (response: AnimalResponse) => {
+        this.registeredAnimal = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  private enumValidator(enumRef: any): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const value = control.value;
+      const isValid = Object.values(enumRef).includes(value);
+      return isValid ? null : { enum: { invalidValue: value } };
+    };
+  }
 }
