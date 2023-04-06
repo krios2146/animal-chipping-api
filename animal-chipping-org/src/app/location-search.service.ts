6179021e1ed6f03c5b397f4ea5db70a4f662ab56
Vladimir Kidyaev <krios2146@gmail.com>
2023-04-06 18:14:46 +0700

    feat: add logic to the location-search service

--- animal-chipping-org/src/app/location-search.service.ts
+++ animal-chipping-org/src/app/location-search.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { VisitedLocation } from './visited-location/visited-location';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocationSearchService {
+  private apiUrl = 'http://78.153.130.215:8080/animals';
+  private email: string = "root@email.com";
+  private password: string = "root";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  search(animalId: number, startDateTime: string, endDateTime: string) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Authorization': 'Basic ' + btoa(`${this.email}:${this.password}`)
+      })
+    };
+
+    return this.http.get<VisitedLocation[]>(`${this.apiUrl}/${animalId}/locations?startDateTime=${startDateTime}&endDateTime=${endDateTime}`, httpOptions);
+  }
 }
