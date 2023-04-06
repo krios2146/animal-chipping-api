a2b736f24fb26173b3b7a5e79ffab530acf68c3b
Vladimir Kidyaev <krios2146@gmail.com>
2023-04-06 18:15:08 +0700

    feat: fill location-search component with content

--- animal-chipping-org/src/app/location-search/location-search.component.ts
+++ animal-chipping-org/src/app/location-search/location-search.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LocationSearchService } from '../location-search.service';
+import { VisitedLocation } from '../visited-location/visited-location';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-location-search',
@@ -6,5 +10,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./location-search.component.css']
 })
 export class LocationSearchComponent {
+  locationSearchForm: FormGroup;
 
+  foundedLocations: VisitedLocation[] | undefined;
+
+  constructor(private formBuilder: FormBuilder, private locationSearchService: LocationSearchService) {
+    this.locationSearchForm = this.formBuilder.group({
+      animalId: [null, [Validators.required, Validators.min(1)]],
+      startDateTime: [null, [Validators.required]],
+      endDateTime: [null, [Validators.required]],
+    });
+  }
+
+  search() {
+    const animalId = this.locationSearchForm.get('animalId')?.value;
+    const startDate = this.locationSearchForm.get('startDateTime')?.value;
+    const endDate = this.locationSearchForm.get('endDateTime')?.value;
+
+    const startDateTime = this.convertToISODate(startDate);
+    const endDateTime = this.convertToISODate(endDate);
+
+    this.locationSearchService.search(animalId, startDateTime, endDateTime).subscribe(
+      (response: VisitedLocation[]) => {
+        this.foundedLocations = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  private convertToISODate(dateString: string): string {
+    const date = new Date(dateString);
+    const isoDate = date.toISOString();
+    return isoDate;
+  }
 }
