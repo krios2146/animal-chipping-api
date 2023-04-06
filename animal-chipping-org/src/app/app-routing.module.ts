7eab204f6e7378d80707e35e596a2fef18fb17bd
Vladimir Kidyaev <krios2146@gmail.com>
2023-04-06 15:28:38 +0700

    feat: add path to the location-search component

--- animal-chipping-org/src/app/app-routing.module.ts
+++ animal-chipping-org/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { AnimalRegistrationComponent } from './animal-registration/animal-registration.component';
+import { LocationSearchComponent } from './location-search/location-search.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'registration', component: RegistrationComponent },
   { path: 'animal-registration', component: AnimalRegistrationComponent },
+  { path: 'location-search', component: LocationSearchComponent },
 ];
 
 @NgModule({
