23ca3680e11cfbace35905a05cc1f1fd883cf4b7
Vladimir Kidyaev <krios2146@gmail.com>
2023-04-06 18:34:55 +0700

    feat: add convertation from ISO date time to "human readable"

--- animal-chipping-org/src/app/location-search/location-search.component.ts
+++ animal-chipping-org/src/app/location-search/location-search.component.ts
@@ -32,6 +32,9 @@ export class LocationSearchComponent {
 
     this.locationSearchService.search(animalId, startDateTime, endDateTime).subscribe(
       (response: VisitedLocation[]) => {
+        response.forEach(location => {
+          location.dateTimeOfVisitLocationPoint = this.convertFromISODate(location.dateTimeOfVisitLocationPoint);
+        });
         this.foundedLocations = response;
       },
       (error: HttpErrorResponse) => {
@@ -45,4 +48,12 @@ export class LocationSearchComponent {
     const isoDate = date.toISOString();
     return isoDate;
   }
+
+  private convertFromISODate(dateString: string): string {
+    const date = new Date(dateString);
+    const year = date.getFullYear().toString().padStart(4, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  }  
 }
