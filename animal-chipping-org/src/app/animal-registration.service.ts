381f5a31200115b94ac2d579a22752570d75136c
Vladimir Kidyaev <krios2146@gmail.com>
2023-04-06 09:50:11 +0700

    fix: hardcode root user credentials

--- animal-chipping-org/src/app/animal-registration.service.ts
+++ animal-chipping-org/src/app/animal-registration.service.ts
@@ -2,24 +2,18 @@ import { Injectable } from '@angular/core';
 import { AnimalCreateRequest } from './animal/animal-create-request';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { AnimalResponse } from './animal/animal-response';
-import { DataShareService } from './data-share.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AnimalRegistrationService {
   private apiUrl = 'http://78.153.130.215:8080/animals';
-  private email: string | undefined;
-  private password: string | undefined;
+  private email: string = "root@email.com";
+  private password: string = "root";
 
-  constructor(private http: HttpClient, private dataShareService: DataShareService) {
-    dataShareService.email.subscribe(email => this.email = email);
-    dataShareService.password.subscribe(password => this.password = password);
-  }
+  constructor(private http: HttpClient) {}
 
   registerAnimal(animal: AnimalCreateRequest) {
-    console.log(this.email);
-    console.log(this.password);
     const httpOptions = {
       headers: new HttpHeaders({
         'Authorization': 'Basic ' + btoa(`${this.email}:${this.password}`)
