59dcba25fc1a9d0d1f4278d3026e2b86746da146
Vladimir Kidyaev <krios2146@gmail.com>
2023-04-07 16:23:47 +0700

    feat: add new AnimalVisitedLocationFrontDto

---
+++ src/main/java/com/animalchipping/api/entity/dto/AnimalVisitedLocationFrontDto.java
@@ -0,0 +1,10 @@
+package com.animalchipping.api.entity.dto;
+
+import java.time.OffsetDateTime;
+
+public class AnimalVisitedLocationFrontDto {
+    private Long id;
+    private OffsetDateTime dateTimeOfVisitLocationPoint;
+    private Long latitude;
+    private Long longitude;
+}
