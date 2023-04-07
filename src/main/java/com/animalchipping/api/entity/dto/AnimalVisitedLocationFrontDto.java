69caaf325bb7b4540aaf98c52df68b1efc824955
Vladimir Kidyaev <krios2146@gmail.com>
2023-04-07 16:26:43 +0700

    fix: add constructor & type of fields

--- src/main/java/com/animalchipping/api/entity/dto/AnimalVisitedLocationFrontDto.java
+++ src/main/java/com/animalchipping/api/entity/dto/AnimalVisitedLocationFrontDto.java
@@ -1,10 +1,15 @@
 package com.animalchipping.api.entity.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 import java.time.OffsetDateTime;
 
+@Data
+@AllArgsConstructor
 public class AnimalVisitedLocationFrontDto {
     private Long id;
     private OffsetDateTime dateTimeOfVisitLocationPoint;
-    private Long latitude;
-    private Long longitude;
+    private Double latitude;
+    private Double longitude;
 }
