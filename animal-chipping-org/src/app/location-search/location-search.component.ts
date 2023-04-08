51b26eb9054dc187e35188274f281d46dc1d9ef0
Vladimir Kidyaev <krios2146@gmail.com>
2023-04-08 09:23:34 +0700

    feat: add time parsing to the location-search component

--- animal-chipping-org/src/app/location-search/location-search.component.ts
+++ animal-chipping-org/src/app/location-search/location-search.component.ts
@@ -54,6 +54,8 @@ export class LocationSearchComponent {
     const year = date.getFullYear().toString().padStart(4, '0');
     const month = (date.getMonth() + 1).toString().padStart(2, '0');
     const day = date.getDate().toString().padStart(2, '0');
-    return `${year}-${month}-${day}`;
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    return `${year}-${month}-${day} ${hours}:${minutes}`;
   }  
 }
